d2c825ce37b7ef1766fef9a5769237d6197d3085
Голубев <ged@mosremit.ru>
2025-01-30 10:18:45 +0300

    Ready task number 3 v1

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,19 +9,6 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        addSubview(imgProfile)
-        addSubview(fullNameLabel)
-        addSubview(buttonStatus)
-        addSubview(infoUser)
-        addSubview(buttonTitle)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     let imgProfile: UIImageView = {
         let imageView = UIImageView(image: UIImage(resource: .avatarka))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -72,7 +59,7 @@ class ProfileHeaderView: UIView {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .systemBlue
-        button.setTitle("Snow status", for: .normal)
+        button.setTitle("Button title", for: .normal)
         button.layer.shadowOffset = CGSize(width: 4.00, height: 4.00)
         button.layer.shadowRadius = 4
         button.layer.cornerRadius = 4
@@ -83,8 +70,80 @@ class ProfileHeaderView: UIView {
     }()
     
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imgProfile)
+        addSubview(fullNameLabel)
+        addSubview(buttonStatus)
+        addSubview(infoUser)
+        addSubview(buttonTitle)
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     @objc func printStatus() {
         print("Status: \(infoUser.text!)")
    }
+    
+    private func setupConstraints(){
+        
+        let safeZone = self.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            
+            // Фото профиля
+            
+            //Высота объекта
+            imgProfile.heightAnchor.constraint(equalToConstant: 128.00),
+            
+            //Длина объекта
+            imgProfile.widthAnchor.constraint(equalToConstant: 128.00),
+            
+            //Горизонтальная линия по безопасной зоне
+            //ProfileHeader.imgProfile.centerXAnchor.constraint(equalTo: safeZone.centerXAnchor),
+            
+              //Вертикальная линия по безопасной зоне
+              //ProfileHeader.imgProfile.centerYAnchor.constraint(equalTo: safeZone.centerYAnchor,),
+            
+            //Отсуп слева
+            imgProfile.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
+            
+            //Отступ сверху от безопасной зоны
+            imgProfile.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 16),
+            
+            
+            //Имя профиля
+
+            fullNameLabel.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 150),
+            fullNameLabel.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 27),
+
+
+            //Описание профиля
+
+            infoUser.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 150),
+            infoUser.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 100),
+
+
+            //Кнопка статуса
+
+            buttonStatus.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 160),
+            buttonStatus.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 16),
+            buttonStatus.centerXAnchor.constraint(equalTo: safeZone.centerXAnchor),
+
+            
+            //Кнопка заголовка
+
+            buttonTitle.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 0),
+            buttonTitle.rightAnchor.constraint(equalTo: safeZone.rightAnchor, constant: 0),
+            buttonTitle.bottomAnchor.constraint(equalTo: safeZone.bottomAnchor, constant: 32)
+        
+            
+        ])
+    }
+
+    
     
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    let ProfileHeader: ProfileHeaderView = ProfileHeaderView()
+    let profileHeader: ProfileHeaderView = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         title = "Profile"
-        self.view.addSubview(ProfileHeader)
+        profileHeader.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(profileHeader)
         setupConstraints()
 
     }
     
-    override func viewWillLayoutSubviews() {
-        ProfileHeader.frame = view.frame
-        
-    }
     private func setupConstraints(){
         
         let safeZone = view.safeAreaLayoutGuide
@@ -31,57 +28,10 @@ class ProfileViewController: UIViewController {
             
             //Основная Вью
             
-            ProfileHeader.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
-            ProfileHeader.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
-            ProfileHeader.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 0),
-            ProfileHeader.heightAnchor.constraint(equalToConstant: 220),
-            
-            
-            // Фото профиля
-            
-            //Высота объекта
-            ProfileHeader.imgProfile.heightAnchor.constraint(equalToConstant: 128.00),
-            
-            //Длина объекта
-            ProfileHeader.imgProfile.widthAnchor.constraint(equalToConstant: 128.00),
-            
-            //Горизонтальная линия по безопасной зоне
-            //ProfileHeader.imgProfile.centerXAnchor.constraint(equalTo: safeZone.centerXAnchor),
-            
-              //Вертикальная линия по безопасной зоне
-              //ProfileHeader.imgProfile.centerYAnchor.constraint(equalTo: safeZone.centerYAnchor,),
-            
-            //Отсуп слева
-            ProfileHeader.imgProfile.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
-            
-            //Отступ сверху от безопасной зоны
-            ProfileHeader.imgProfile.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 16),
-            
-            
-            //Имя профиля
-
-            ProfileHeader.fullNameLabel.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 150),
-            ProfileHeader.fullNameLabel.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 27),
-            
-            
-            //Описание профиля
-            
-            ProfileHeader.infoUser.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 150),
-            ProfileHeader.infoUser.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 100),
-            
-            
-            //Кнопка статуса
-            
-            ProfileHeader.buttonStatus.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 16),
-            ProfileHeader.buttonStatus.centerXAnchor.constraint(equalTo: safeZone.centerXAnchor),
-            ProfileHeader.buttonStatus.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 160),
-            
-            //Кнопка заголовка
-            
-            ProfileHeader.buttonTitle.leftAnchor.constraint(equalTo: safeZone.leftAnchor, constant: 0),
-            ProfileHeader.buttonTitle.rightAnchor.constraint(equalTo: safeZone.rightAnchor, constant: 0),
-            ProfileHeader.buttonTitle.bottomAnchor.constraint(equalTo: safeZone.bottomAnchor, constant: -16)
-            
+            profileHeader.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            profileHeader.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            profileHeader.topAnchor.constraint(equalTo: safeZone.topAnchor, constant: 0),
+            profileHeader.heightAnchor.constraint(equalToConstant: 220),
             
         ])
     }
